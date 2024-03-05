07deb7f36a830321b543fff93148a39dd379bece
shu <mrkmsm1009@gmail.com>
2024-03-06 00:42:16 +0900

    sqlite3に対応

--- README.md
+++ README.md
@@ -4,22 +4,8 @@
 高専祭運営が使うWebApp。
 
 ## Install
-mysql、nginx、node.jsをインストールする。  
-`$ sudo apt install -y mysql-server nginx nodejs npm`
-
-### mysql
-最初にセキュリティスクリプトを実行する。  
-`$ sudo mysql_secure_installation`  
-色々聞かれるけど、だいたいYesでいいと思う。  
-
-次に、`$ sudo mysql`でrootでログインする。  
-mysqlのuserを作成する。  
-```sql
-> CREATE USER 'new user'@'localhost' IDENTIFIED BY 'new user password';
-> GRANT ALL PRIVILEGES ON * . * TO 'new user'@'localhost';
-> FLUSH PRIVILEGES;
-> ALTER USER 'new user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'new user password';
-```
+nginx、node.jsをインストールする。  
+`$ sudo apt install -y nginx nodejs npm`
 
 ### node.js
 node jsのインストールはちょっとクセがある。  
@@ -35,13 +21,6 @@ $ sudo apt purge -y nodejs npm
 サーバサイドは、M2pro macmini、i7 MBP、ubuntu serverで確認済み。  
 クライアントサイドは、iPhone XR、iPhone 7、Xiaomi Mi 11 Lite 5Gで確認済み。  
 ### サーバ側
-mysqlにdatabaseとtableを作る。  
-database名とtable名を変えてもいいが、もれなくコードも変える必要が出てくるので、このままを推奨。  
-```sql
-> CREATE DATABASE counter;
-> USE counter;
-> CREATE TABLE users (id INT AUTO_INCREMENT, date TEXT, count INT UNSIGNED, PRIMARY KEY (id));
-```  
 nginx.confにlocationを追記する。  
 ```nginx
 location /counter/ {
@@ -54,7 +33,6 @@ location /counter/ {
 }
 ```
 git cloneしてディレクトリ移動して、`$ npm install`。  
-envファイルに上で設定したデータベースのログイン項目を書く。  
 `$ node server.js`で走らせる。  
 カウンタのurlは、http(s)://<ドメイン>/counter/  
 管理画面のurlは、http(s)://<ドメイン>/counter/mgmt/  
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const mysql = require("mysql");
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("counter.db");
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
@@ -7,61 +8,55 @@ const server = http.Server(app);
 const io = socketio(server);
 const fs = require('fs');
 const PORT = process.env.PORT || 3001;
-require("dotenv").config();
 let status = false;
 
-app.get("/", (req, res) => {
+app.get("/", function(req, res){
     res.sendFile(__dirname + "/src/index.html");
 });
-app.get("/mgmt", (req, res) => {
+app.get("/mgmt", function(req, res){
     res.sendFile(__dirname + "/src/management.html");
 });
 
 app.use(express.static(__dirname + "/src"));
 app.use(express.static(__dirname));
-
-server.listen(PORT, () => {
+server.listen(PORT, function(){
     console.log("server on port %d", PORT);
 });
 
-/* データベース接続 */
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-connection.connect((err) => {
-    if(err){
-        console.log('errpr connecting: ' + err.stack);
-        return;
-    }
-    console.log('success');
-});
-
-io.on("connection", (socket) => {
+io.on("connection", function(socket){
     // console.log("connection: ", socket.id);
     socket.on("no_cookie", function(){ //counterの個別id生成，登録
         let date = new Date();
-        let today = date.getMonth() + 1 + "/" + date.getDate();
-        let newUser = {
-            date: today,
-            count: 0
-        };
-        connection.query('INSERT INTO users SET ?', newUser, (err, res) => {
+        let today = (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getDate().toString().padStart(2, "0");
+        db.run("INSERT INTO users (date, count) VALUES (?, ?)", today, 0, function(err){
             if(err) throw err;
-            socket.emit("id_cookie", res.insertId);
+            socket.emit("id_cookie", this.lastID);
         });
     });
+
     socket.on("get_own_value", function(data){ //再読み込みで自分のカウントを送信
-        connection.query('SELECT * FROM users WHERE id = ?', [data.id], (err, result) => {
-            socket.emit("own_num", result[0].count);
+        db.all("SELECT * FROM users WHERE id=?", data.id, function(err, rows){
+            let date = new Date();
+            let today = (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getDate().toString().padStart(2, "0");
+            let ownCount = 0;
+            if(rows[0].date != today){
+                let date = new Date();
+                let today = (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getDate().toString().padStart(2, "0");
+                db.run("INSERT INTO users (date, count) VALUES (?, ?)", today, 0, function(err){
+                    if(err) throw err;
+                    socket.emit("id_cookie", this.lastID);
+                });
+            }else{
+                ownCount = rows[0].count;
+            }
+            socket.emit("own_num", ownCount);
         });
     });
+
     socket.on("admission", async function(data){ //counter加算
         if(!status){
-            let  masterData = JSON.parse(fs.readFileSync("./admission_control.json", "utf8"));
             status = true;
+            let  masterData = JSON.parse(fs.readFileSync("./admission_control.json", "utf8"));
             let date = new Date();
             date.setHours(date.getHours());
             // date.setHours(date.getHours() + 9);
@@ -73,56 +68,46 @@ io.on("connection", (socket) => {
             fs.writeFileSync("admission_control.json", JSON.stringify(masterData, null, "    "));
             await number_of_peple_per_hour();
         }
-        connection.query('UPDATE users SET count = ? Where id = ?',
-            [data.count, data.id],
-            (err, result) => {
-                if(err) throw err;
-            }
-        );
+        db.run("UPDATE users SET count=? WHERE id=?", data.count, data.id);
     });
+
     socket.on("get_history", function(){ //managementへ総入場者数と履歴送信
         let json_ally = JSON.parse(fs.readFileSync("./admission_control.json", "utf8"));
         socket.emit("send_start_hour", {hour: json_ally.begin_time.hour, min: json_ally.begin_time.min, per_hour_data: json_ally.per_hour});
-        connection.query('SELECT * FROM users', (err, result) => {
+        db.all("SELECT * FROM users", function(err, rows){
             let count_admission = 0;
             let data = [];
             let date = new Date();
-            let today = date.getMonth() + 1 + "/" + date.getDate();
-            // console.log(result);
-            for(let i = 0; i < result.length; i++){
-                if(result[i].date == today){
-                    count_admission += result[i].count;
-                }else{
-                    if(data.length != 0){
-                        let length = data.length
-                        for(let j = 0; j < length; j++){
-                            if(data[j].date == result[i].date){
-                                data[j].count += result[i].count;
-                                break;
-                            }else if(j == data.length - 1){
-                                data.push({date: result[i].date, count: result[i].count});
-                            }
+            let today = (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getDate().toString().padStart(2, "0");
+            for(let i = 0; i < rows.length; i++){
+                if(rows[i].date == today){
+                    count_admission += rows[i].count;
+                }else if(data.length != 0){
+                    let length = data.length
+                    for(let j = 0; j < length; j++){
+                        if(data[j].date == rows[i].date){
+                            data[j].count += rows[i].count;
+                            break;
+                        }else if(j == data.length - 1){
+                            data.push({date: rows[i].date, count: rows[i].count});
                         }
-                    }else{
-                        data.push({date: result[i].date, count: result[i].count});
                     }
-                    
+                }else{
+                    data.push({date: rows[i].date, count: rows[i].count});
                 }
             }
             socket.emit("all_num", count_admission);
             socket.emit("history_data", data);
         });
     });
+
     socket.on("get_all_count", function(){
-        connection.query('SELECT * FROM users', (err, result) => {
+        db.all("SELECT * FROM users", function(err, rows){
             let count_admission = 0;
             let date = new Date();
-            let today = date.getMonth() + 1 + "/" + date.getDate();
-            // console.log(result);
-            for(let i = 0; i < result.length; i++){
-                if(result[i].date == today){
-                    count_admission += result[i].count;
-                }
+            let today = (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getDate().toString().padStart(2, "0");
+            for(let i = 0; i < rows.length; i++){
+                if(rows[i].date == today) count_admission += rows[i].count;
             }
             socket.emit("all_num", count_admission);
         });
@@ -130,24 +115,18 @@ io.on("connection", (socket) => {
 });
 
 let per_time = 1000*60*60; //1h
-// let per_time = 1000 * 5;
 async function number_of_peple_per_hour(){
     let per_hour_count = 0;
-    let interval = setInterval(() => {
+    let interval = setInterval(function(){
         let date = new Date();
-        let today = date.getMonth() + 1 + "/" + date.getDate();
+        let today = (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getDate().toString().padStart(2, "0");
         let data_per_hour = JSON.parse(fs.readFileSync("./admission_control.json", "utf8"));
         let value = 0;
-        if(!"per_hour" in data_per_hour){
-            data_per_hour.per_hour = [];
-        }
-        connection.query('SELECT * FROM users', (err, result) => {
-            for(let i = 0; i < result.length; i++){
-                if(result[i].date == today){
-                    value += result[i].count;
-                }
+        if(!"per_hour" in data_per_hour) data_per_hour.per_hour = [];
+        db.all("SELECT * RFOM users", function(err, rows){
+            for(let i = 0; i < rows.length; i++){
+                if(rows[i].date == today) value += rows[i].count;
             }
-            // console.log("total: ", value);
     
             let total_per_hour = 0;
             if(data_per_hour.per_hour.length != 0){
@@ -156,14 +135,12 @@ async function number_of_peple_per_hour(){
                 }
             }
     
-            // console.log("per: ", total_per_hour);
             let diff = value - total_per_hour;
-            // console.log("diff: ", diff);
             data_per_hour.per_hour.push(diff);
             io.emit("number_of_people_per_hour", {index: per_hour_count, number_of_people: diff});
             fs.writeFileSync("./admission_control.json", JSON.stringify(data_per_hour, null, "    "));
             per_hour_count++;
-            //毎時間の人数を出す，各時間帯をjsonに保存しておく(管理画面から読み込んでも見れるようにする)，全部の合計からこれまでの時間帯の合計を引く．この関数が呼び出された時各時間帯jsonを初期化する．
+            //毎時間の人数を出す
             if(per_hour_count > 7){
                 clearInterval(interval);
                 interval = null;
